5064af3675bad2480f63af80ef0811f4f897d3db
Kwan Lin <kwan@alumni.brown.edu>
2018-01-17 01:17:38 -0500

    Fix path for get_account

--- R/get_account.R
+++ R/get_account.R
@@ -8,7 +8,7 @@
 #' get_account(some_auth_key, some_account_id)
 
 get_account <- function(auth_key, account_id) {
-  url <- paste0(c("http://app.pendo.io/api/v1/account/",account_id,"/results.json"), collapse='')
+  url <- paste0(c("https://app.pendo.io/api/v1/account/",account_id), collapse='')
   account <- httr::GET(url,
                        httr::add_headers("X-Pendo-Integration-Key" = auth_key,
                                          "Content-Type" = "application/json")
